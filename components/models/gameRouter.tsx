1ad17cd0bbf407f9e181986918dd9091d7d12452
Kortney Wright <mithreegals1@yahoo.com>
2026-02-26 18:08:42 -0500

    flashing on mobile gone due to renavigating to play view once on playview

--- components/models/gameRouter.tsx
+++ components/models/gameRouter.tsx
@@ -4,15 +4,15 @@ import { Redirect } from 'expo-router';
 
 export default function GameRouter() {
     const { state } = useGame();
-
+    console.log('game router is being used')
     if (!state) return null;
 
     switch (state.gamePhase) {
         case 'setup':
             return <Redirect href="(tabs)" />;
 
-        case 'playing':
-            return <Redirect href="play" />;
+        //case 'playing':
+        //    return <Redirect href="play" />;
 
         case 'gameOver':
             return <Redirect href="gameResult" />;

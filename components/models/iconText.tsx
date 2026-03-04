582b26603df7bbf24cde075d18dae4d4fcdb4664
Kortney Wright <mithreegals1@yahoo.com>
2026-03-04 16:54:40 -0500

    icons have color on gameplay view

--- components/models/iconText.tsx
+++ components/models/iconText.tsx
@@ -2,6 +2,7 @@ import { Text, View } from 'react-native';
 import { StyleSheet, Pressable } from 'react-native';
 import { IconSymbol } from '../ui/icon-symbol';
 import { theme } from '../../theme/theme';
+import { SymbolViewProps } from 'expo-symbols';
 
 interface IconTextProps {
     iconFirst: boolean;
@@ -10,17 +11,34 @@ interface IconTextProps {
     altColor?: string;
 }
 
+const iconColorMapping: IconMapping = {
+    'hare.fill': 'green',
+    'staroflife.fill': 'gray',
+    'chart.bar.fill': 'purple',
+};
+
+type IconMapping = Partial<Record<SymbolViewProps['name'], string>>;
+
 export function IconText({ iconFirst, text, iconName, altColor}: IconTextProps) {
     // TODO: think if we want icon colors to align with type - shuriken gray, level orange, bunny blue, thumbs up?
-    if (altColor === undefined || altColor === '') {
-        altColor = theme.color.gameplayIcon.text;
-    }
+
+    // use for all white
+    //if (altColor === undefined || altColor === '') {
+    //    altColor = theme.color.gameplayIcon.text;
+    //}
+
+    // use for colored icons & add to return () 
+    const resolvedColor =
+        altColor && altColor !== ''
+            ? altColor
+            : iconColorMapping[iconName] ?? theme.color.gameplayIcon.text 
+    
     const iconSize = iconFirst ? 28 : 24;
     return (
         <View style={styles.container} >
-            {!iconFirst && <Text style={[styles.textRight, { color: altColor }]}>{text}</Text>}
-            <IconSymbol size={iconSize} name={iconName} color={altColor} />
-            {iconFirst && <Text style={[styles.textLeft, { color: altColor }]}>{text}</Text>}
+            {!iconFirst && <Text style={[styles.textRight, { color: altColor || theme.color.gameplayIcon.text }]}>{text}</Text>}
+            <IconSymbol size={iconSize} name={iconName} color={resolvedColor} />
+            {iconFirst && <Text style={[styles.textLeft, { color: altColor || theme.color.gameplayIcon.text }]}>{text}</Text>}
         </View>
     )
 }
